a9d962aa13a960424c7d9eee00dc10bb9bf1744f
fouric <fouric@protonmail.com>
2018-05-20 12:10:36 -0700

    fix indentation some

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-
 /*
  * WS2812B: Drive a single WS2812B using SPI.
  * Serial interface is included for debugging purposes
-*/
+ */
 
 
 #include "stm32f042.h"
@@ -10,147 +9,137 @@
 #include "spi.h"
 void delay(int);
 
-void delay(int dly)
-{
+void delay(int dly) {
   while( dly--);
 }
 
-
-
-void configPins()
-{
-	// Power up PORTB
-	RCC_AHBENR |= BIT18;
-	GPIOB_MODER |= BIT6; // make bit3  an output
-	GPIOB_MODER &= ~BIT7; // make bit3  an output
+void configPins() {
+  // Power up PORTB
+  RCC_AHBENR |= BIT18;
+  GPIOB_MODER |= BIT6; // make bit3  an output
+  GPIOB_MODER &= ~BIT7; // make bit3  an output
 }
 
-void writeWS2812B(unsigned long Value)
-{
-    // have to expand each bit to 3 bits
-    // Can then output 110 for WS2812B logic '1'
-    // and 100 for WS2812B logic '0'
-    uint32_t Encoding=0;
-    uint8_t SPI_Data[9];
-    int Index;
+void writeWS2812B(unsigned long Value) {
+  // have to expand each bit to 3 bits
+  // Can then output 110 for WS2812B logic '1'
+  // and 100 for WS2812B logic '0'
+  uint32_t Encoding=0;
+  uint8_t SPI_Data[9];
+  int Index;
 
-    // Process the GREEN byte
-    Index=0;
-    Encoding=0;
-    while (Index < 8)
+  // Process the GREEN byte
+  Index=0;
+  Encoding=0;
+  while (Index < 8)
     {
-        Encoding = Encoding << 3;
-        if (Value & BIT23)
+      Encoding = Encoding << 3;
+      if (Value & BIT23)
         {
-            Encoding |= 0b110;
+          Encoding |= 0b110;
         }
-        else
+      else
         {
-            Encoding |= 0b100;
+          Encoding |= 0b100;
         }
-        Value = Value << 1;
-        Index++;
+      Value = Value << 1;
+      Index++;
 
     }
-    SPI_Data[0] = ((Encoding >> 16) & 0xff);
-    SPI_Data[1] = ((Encoding >> 8) & 0xff);
-    SPI_Data[2] = (Encoding & 0xff);
+  SPI_Data[0] = ((Encoding >> 16) & 0xff);
+  SPI_Data[1] = ((Encoding >> 8) & 0xff);
+  SPI_Data[2] = (Encoding & 0xff);
 
-    // Process the RED byte
-    Index=0;
-    Encoding=0;
-    while (Index < 8)
+  // Process the RED byte
+  Index=0;
+  Encoding=0;
+  while (Index < 8)
     {
-        Encoding = Encoding << 3;
-        if (Value & BIT23)
+      Encoding = Encoding << 3;
+      if (Value & BIT23)
         {
-            Encoding |= 0b110;
+          Encoding |= 0b110;
         }
-        else
+      else
         {
-            Encoding |= 0b100;
+          Encoding |= 0b100;
         }
-        Value = Value << 1;
-        Index++;
+      Value = Value << 1;
+      Index++;
 
     }
-    SPI_Data[3] = ((Encoding >> 16) & 0xff);
-    SPI_Data[4] = ((Encoding >> 8) & 0xff);
-    SPI_Data[5] = (Encoding & 0xff);
+  SPI_Data[3] = ((Encoding >> 16) & 0xff);
+  SPI_Data[4] = ((Encoding >> 8) & 0xff);
+  SPI_Data[5] = (Encoding & 0xff);
 
-    // Process the BLUE byte
-    Index=0;
-    Encoding=0;
-    while (Index < 8)
-    {
-        Encoding = Encoding << 3;
-        if (Value & BIT23)
-        {
-            Encoding |= 0b110;
-        }
-        else
-        {
-            Encoding |= 0b100;
-        }
-        Value = Value << 1;
-        Index++;
-
+  // Process the BLUE byte
+  Index=0;
+  Encoding=0;
+  while (Index < 8) {
+    Encoding = Encoding << 3;
+    if (Value & BIT23) {
+      Encoding |= 0b110;
+    } else {
+      Encoding |= 0b100;
     }
-    SPI_Data[6] = ((Encoding >> 16) & 0xff);
-    SPI_Data[7] = ((Encoding >> 8) & 0xff);
-    SPI_Data[8] = (Encoding & 0xff);
+    Value = Value << 1;
+    Index++;
 
-    // Now send out the 24 (x3) bits to the SPI bus
-    writeSPI(SPI_Data,9);
+  }
+  SPI_Data[6] = ((Encoding >> 16) & 0xff);
+  SPI_Data[7] = ((Encoding >> 8) & 0xff);
+  SPI_Data[8] = (Encoding & 0xff);
+
+  // Now send out the 24 (x3) bits to the SPI bus
+  writeSPI(SPI_Data,9);
 
 }
-unsigned long getRainbow()
-{   // Cycle through the colours of the rainbow (non-uniform brightness however)
-	// Inspired by : http://academe.co.uk/2012/04/arduino-cycling-through-colours-of-the-rainbow/
-	static unsigned Red = 255;
-	static unsigned Green = 0;
-	static unsigned Blue = 0;
-	static int State = 0;
-	switch (State)
-	{
-		case 0:{
-			Green++;
-			if (Green == 255)
-				State = 1;
-			break;
-		}
-		case 1:{
-			Red--;
-			if (Red == 0)
-				State = 2;
-			break;
-		}
-		case 2:{
-			Blue++;
-			if (Blue == 255)
-				State = 3;
-			break;
-		}
-		case 3:{
-			Green--;
-			if (Green == 0)
-				State = 4;
-			break;
-		}
-		case 4:{
-			Red++;
-			if (Red == 255)
-				State = 5;
-			break;
-		}
-		case 5:{
-			Blue --;
-			if (Blue == 0)
-				State = 0;
-			break;
-		}
-	}
-	return (Green << 16) + (Red << 8) + Blue;
+
+unsigned long getRainbow() {   // Cycle through the colours of the rainbow (non-uniform brightness however)
+  // Inspired by : http://academe.co.uk/2012/04/arduino-cycling-through-colours-of-the-rainbow/
+  static unsigned Red = 255;
+  static unsigned Green = 0;
+  static unsigned Blue = 0;
+  static int State = 0;
+  switch (State) {
+  case 0:{
+    Green++;
+    if (Green == 255)
+      State = 1;
+    break;
+  }
+  case 1:{
+    Red--;
+    if (Red == 0)
+      State = 2;
+    break;
+  }
+  case 2:{
+    Blue++;
+    if (Blue == 255)
+      State = 3;
+    break;
+  }
+  case 3:{
+    Green--;
+    if (Green == 0)
+      State = 4;
+    break;
+  }
+  case 4:{
+    Red++;
+    if (Red == 255)
+      State = 5;
+    break;
+  }
+  case 5:{
+    Blue --;
+    if (Blue == 0)
+      State = 0;
+    break;
+  }
+  }
+  return (Green << 16) + (Red << 8) + Blue;
 }
 
 
@@ -192,20 +181,19 @@ void initClock() {
 }
 
 int main() {
-	//unsigned count=0;
+  //unsigned count=0;
 
   initClock();
 
-	initUART(9600);  // Set serial port to 9600,n,8,1
-	configPins(); // Set up the pin to drive the onboard LDE
-	initSPI(); // set up the SPI bus
+  initUART(9600);  // Set serial port to 9600,n,8,1
+  configPins(); // Set up the pin to drive the onboard LDE
+  initSPI(); // set up the SPI bus
 
-	while(1)
-	{
-		GPIOB_ODR |= BIT3;	// LED on
-		writeWS2812B(getRainbow()); // Output a colour Format: GGRRBB
-		delay(5000); // Wait for a while so we can see it
-		GPIOB_ODR &= ~BIT3; // LED off
-	}
-	return 0;
+  while(1) {
+    GPIOB_ODR |= BIT3;	// LED on
+    writeWS2812B(getRainbow()); // Output a colour Format: GGRRBB
+    delay(5000); // Wait for a while so we can see it
+    GPIOB_ODR &= ~BIT3; // LED off
+  }
+  return 0;
 }
